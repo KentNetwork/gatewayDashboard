30df84e9cfd001e7a3cdc22c2e0a87c52eda3ce7
danielknox <dk242@kent.ac.uk>
2018-04-15 14:01:34 +0100

    current framework

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Dashboard' });
+});
+
+router.get('/activity', function(req, res, next) {
+  res.render('activity', { title: 'Gateway Activity' });
+});
+
+router.get('/coverage', function(req, res, next) {
+  res.render('coverage', { title: 'Network Coverage' });
 });
 
 module.exports = router;
